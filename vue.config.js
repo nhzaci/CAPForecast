37ca8e17f0ca69c1569188cb6703e221db88adc3
Zack <warrior.killz@gmail.com>
2020-06-09 17:02:57 +0800

    Corrected vueconfig directory

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
-  publicPath: process.env.NODE_ENV === 'production' ? '/ZacksSiteVue/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/CAPForecast/' : '/'
 }
