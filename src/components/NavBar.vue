215048546f693aacdcbd43dcee62fafd153904b2
Zack <warrior.killz@gmail.com>
2020-06-09 13:20:55 +0800

    Created application

---
+++ src/components/NavBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app-bar
+    dark
+    app
+  >
+    <v-toolbar-title>NUS SU Calculator</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn
+      text
+      to="/"
+      hidden
+    >
+      Home
+    </v-btn>
+    <v-btn
+      text
+      to="/about"
+      hidden
+    >
+      About
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container class="fill-height" fluid>
+    <v-row
+      justify="center"
+      align="center"
+    >
+      <v-card>
+        <v-card-title>
+          Calculator
+        </v-card-title>
+      </v-card>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
